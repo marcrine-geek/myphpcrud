214a24c542ab8dae86aa2e175b38236701357930
marcrine-geek <marcrinemm@gmail.com>
2022-02-21 09:40:00 +0300

    styling changes

--- edit.php
+++ edit.php
@@ -23,7 +23,6 @@ $theUser = fetchUser($connection, $_GET['id']);
 
 ?>
 
-<h1>hELLO EDIT</h1>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -32,39 +31,69 @@ $theUser = fetchUser($connection, $_GET['id']);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit user's details</title>
-    <link rel="stylesheet" href="./style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <style>
+        label{
+            display: inline-block;
+            width: 100px;
+        }
+    </style>
 </head>
 
 <body>
 
-<h1>This is edit page</h1>
 
-<div class="container">
-    <header class="header">
+<div class="container" style="padding-top: 150px; padding-left: 300px; ">
+    <div class="row">
+        <div class="col-md-8" style="background-color: darkblue; width: 40%; border: 2px solid black; border-radius: 10px;">
+            <form action="" method="post" style="text-align: center;">
+                <h2 style="color: white; padding-top: 15px;">Update <?php echo htmlspecialchars($theUser['name']); ?> Details.</h2>
+                <div style="padding: 15px; color: white;">
+                    <label for="userName">Full Name</label>
+                    <input type="text" name="name" value="<?php echo htmlspecialchars($theUser['name']); ?>" id="userName" placeholder="Name" autocomplete="off" required>
+                </div>
 
-    </header>
-    <div class="wrapper edit-wrapper">
-        <div class="form">
-            <form action="" method="post">
-                <label for="userName">Full Name</label>
-                <input type="text" name="name" value="<?php echo htmlspecialchars($theUser['name']); ?>" id="userName" placeholder="Name" autocomplete="off" required>
-                <label for="userEmail">Email</label>
-                <input type="email" name="email" value="<?php echo htmlspecialchars($theUser['email']); ?>" id="userEmail" placeholder="Email" autocomplete="off" required>
-                <label for="phone">phone</label>
-                <input type="text" name="phone" value="<?php echo htmlspecialchars($theUser['phone']); ?>" id="userName" placeholder="phone" autocomplete="off" required>
-                <label for="address">address</label>
-                <input type="text" name="address" value="<?php echo htmlspecialchars($theUser['address']); ?>" id="address" placeholder="address" autocomplete="off" required>
-                <label for="speciality">speciality</label>
-                <input type="text" name="speciality" value="<?php echo htmlspecialchars($theUser['speciality']); ?>" id="speciality" placeholder="speciality" autocomplete="off" required>
+                <div style="padding: 15px; color: white;">
+                    <label for="userEmail">Email</label>
+                    <input type="email" name="email" value="<?php echo htmlspecialchars($theUser['email']); ?>" id="userEmail" placeholder="Email" autocomplete="off" required>
+                </div>
+
+                <div style="padding: 15px; color: white;">
+                    <label for="phone">phone</label>
+                    <input type="text" name="phone" value="<?php echo htmlspecialchars($theUser['phone']); ?>" id="userName" placeholder="phone" autocomplete="off" required>
+                </div>
+
+                <div style="padding: 15px; color: white;">
+                    <label for="address">address</label>
+                    <input type="text" name="address" value="<?php echo htmlspecialchars($theUser['address']); ?>" id="address" placeholder="address" autocomplete="off" required>
+                </div>
+
+                <div style="padding: 15px; color: white;">
+                    <label for="speciality">speciality</label>
+                    <select name="speciality" value="<?php echo htmlspecialchars($theUser['speciality']); ?>" id="speciality" placeholder="speciality" autocomplete="off" required>
+                        <option value="">--select--</option>
+                        <option value="AI">Artificial Intelligence</option>
+                        <option value="game design">Game Design</option>
+                        <option value="software engineer">Programming Languages</option>
+                        <option value="networks">Networks</option>
+                        <option value="data science">Data Science</option>
+                    </select>
+                </div>
+
                 <?php if (isset($update_data) && $update_data !== true) {
                     echo '<p class="msg err-msg">' . $update_data . '</p>';
                 }
                 ?>
-                <input type="submit" value="Update">
+                <div style="padding: 15px;">
+                    <input type="submit" value="Update" class="btn btn-primary">
+                </div>
+
             </form>
         </div>
     </div>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
 
--- index.php
+++ index.php
@@ -22,63 +22,72 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) &&
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        label{
+            display: inline-block;
+            width: 100px;
+            text-align: center;
+        }
+    </style>
     <title>Registration</title>
 </head>
 <body>
 
-<div class="container">
-    <h1>Registration Form</h1>
+<div class="container" style="padding-top: 60px;">
+
     <div class="row">
-        <div class="col-md-5">
-            <form action="index.php" method="post">
-                <div>
+        <div class="col-md-5" style="background-color: black; border-radius: 10px; width: 40%;">
+            <form action="index.php" method="post" style="padding-top: 30px; color: white;">
+                <h1 style="text-align: center;">Registration Form</h1>
+                <div style="padding: 20px;">
                     <label for="name">Name</label>
                     <input type="text" name="name" placeholder="Enter your name" required>
                 </div>
-                <div>
+                <div style="padding: 20px;">
                     <label for="email">Email</label>
                     <input type="email" name="email" placeholder="Enter your email" required>
                 </div>
-                <div>
+                <div style="padding: 20px;">
                     <label for="phone">Phone</label>
                     <input type="number" name="phone" placeholder="Enter your phone no." required>
                 </div>
-                <div>
+                <div style="padding: 20px;">
                     <label for="address">Address</label>
                     <input type="address" name="address" placeholder="Enter your address" required>
                 </div>
-                <div>
+                <div style="padding: 20px;">
                     <label for="speciality">Speciality</label>
                     <select name="speciality" id="speciality" required>
                         <option value="">--select--</option>
                         <option value="AI">Artificial Intelligence</option>
-                        <option value="games">Game Design</option>
-                        <option value="software">Programming Languages</option>
+                        <option value="game design">Game Design</option>
+                        <option value="software engineer">Programming Languages</option>
                         <option value="networks">Networks</option>
-                        <option value="data">Data Science</option>
+                        <option value="data science">Data Science</option>
                     </select>
                 </div>
                 <?php if (isset($insert_data) && $insert_data !== true) {
                     echo '<p class="msg err-msg">' . $insert_data . '</p>';
                 }
                 ?>
-                <div>
-                    <input type="submit" value="submit">
+                <div style="padding: 20px;">
+                    <input type="submit" value="submit" class="btn btn-primary">
                 </div>
 
             </form>
         </div>
         <div class="col-md-7">
             <?php if (count($all_user) > 0) : ?>
-                <table>
+                <table style="padding-top: 30px; background-color: orange; border: 2px solid black;">
                     <tbody>
-                    <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                        <th>Address</th>
-                        <th>Speciality</th>
-                        <th>Action</th>
+                    <tr style="padding: 20px;">
+                        <th style="padding: 15px;">Name</th>
+                        <th style="padding: 15px;">Email</th>
+                        <th style="padding: 15px;">Phone</th>
+                        <th style="padding: 15px;">Address</th>
+                        <th style="padding: 15px;">Speciality</th>
+                        <th style="padding: 15px;">Action</th>
                     </tr>
                     <?php foreach ($all_user as $user) :
                         $id = $user['id'];
@@ -89,21 +98,21 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) &&
                         $speciality = $user['speciality'];
                         ?>
                         <tr>
-                            <td><?php echo $name; ?></td>
-                            <td><?php echo $email; ?></td>
-                            <td><?php echo $phone; ?></td>
-                            <td><?php echo $address; ?></td>
-                            <td><?php echo $speciality; ?></td>
-                            <td>
-                                <a href="edit.php?id=<?php echo $id; ?>" class="edit">Edit</a>&nbsp;|
-                                <a href="delete.php?id=<?php echo $id; ?>" class="delete delete-action">Delete</a>
+                            <td align="center" style="padding: 15px;"><?php echo $name; ?></td>
+                            <td align="center" style="padding: 15px;"><?php echo $email; ?></td>
+                            <td align="center" style="padding: 15px;"><?php echo $phone; ?></td>
+                            <td align="center" style="padding: 15px;"><?php echo $address; ?></td>
+                            <td align="center" style="padding: 15px;"><?php echo $speciality; ?></td>
+                            <td align="center" style="padding: 15px;">
+                                <a href="edit.php?id=<?php echo $id; ?>" class="btn btn-primary">Edit</a>&nbsp;|
+                                <a href="delete.php?id=<?php echo $id; ?>" class="btn btn-danger">Delete</a>
                             </td>
                         </tr>
                     <?php endforeach; ?>
                     </tbody>
                 </table>
             <?php else : ?>
-                <h2>No records found. Please insert some records.</h2>
+                <h2 style="text-align: center; color: darkblue;">Please insert some records.</h2>
             <?php endif; ?>
         </div>
     </div>
@@ -120,6 +129,6 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) &&
         }
     });
 </script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
